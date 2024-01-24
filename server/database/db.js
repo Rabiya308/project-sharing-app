f20ff8087328c3bc333a04111112f99ca84d7a63
Rabiya <rabiyaimt786@gmail.com>
2024-01-24 13:32:51 +0500

    final commit

---
+++ server/database/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSSWORD = process.env.DB_PASSWORD;
+
+const DBConnection = async () => {
+    const MONGODB_URI =  "mongodb+srv://rabiyaimt786:userpassword@cluster0.zsd2hnv.mongodb.net/?retryWrites=true&w=majority";
+
+    try {
+        await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.error('Error while connecting with database', error.message);
+    }
+}
+
+export default DBConnection;
